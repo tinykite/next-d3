e4cdf8b9040fa1d500c9988dc17660c9d4e4251c
tinykite <dakota@tinykitelab.com>
2023-08-04 14:06:27 -0500

    feat: Initial commit

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,11 @@
+import * as d3 from "d3"
+
+const Timeline = ({ data, xAccessor, yAccessor, label }) => {
+    // const [ref, dimensions] = useChartDimensions()
+    return (
+        <div className="Timeline">
+        </div>
+    )
+}
+
+export default Timeline
